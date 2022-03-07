70d44e4797b214497ee322e3bd4f3167c9077627
Prathamesh Thombre <pthombre657@gmail.com>
2022-03-07 20:59:21 +0530

    Added solution - LeetHub

---
+++ Friends Pairing Problem - GFG/friends-pairing-problem.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    int countFriendsPairings(int n) 
+    { 
+        // f(n) = f(n-1) + (n-1) * f(n-2);
+        
+        // For n-th person there are two choices:
+        // 1) n-th person remains single, we recur for f(n – 1)
+        // 2) n-th person pairs up with any of the
+        // remaining n – 1 persons. We get (n – 1) * f(n – 2)
+
+        int M = 1000000007;
+        vector<long long int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++) {
+            dp[i] = ((dp[i-1] % M) + ((i-1) * dp[i-2])%M) % M;
+        }
+        
+        return dp[n];
+    }
+};    
+ 
+
+// { Driver Code Starts.
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout <<ob.countFriendsPairings(n); 
+    	cout<<endl;
+    }
+} 
+  // } Driver Code Ends
