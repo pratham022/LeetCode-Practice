35a1e926121952724636e615bc01d1f45d42e632
Prathamesh Thombre <pthombre657@gmail.com>
2022-03-11 18:56:01 +0530

    Time: 261 ms (26.82%), Space: 99.7 MB (5.00%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -8,31 +8,34 @@ public:
         stack<int> st1, st2;
         
         for(int i=0; i<heights.size(); i++) {
-            while(!st1.empty() && heights[st1.top()]>=heights[i]) 
+            while(!st1.empty() && heights[st1.top()] >= heights[i])
                 st1.pop();
-            if(st1.empty()) 
+            if(st1.empty())
                 nsl.push_back(-1);
-            else 
+            else
                 nsl.push_back(st1.top());
+            
             st1.push(i);
         }
         
         for(int i=heights.size()-1; i>=0; i--) {
-            while(!st2.empty() && heights[st2.top()]>=heights[i]) 
+            while(!st2.empty() && heights[st2.top()] >= heights[i])
                 st2.pop();
-            if(st2.empty()) 
+            if(st2.empty())
                 nsr.push_back(heights.size());
             else
                 nsr.push_back(st2.top());
+            
             st2.push(i);
         }
         reverse(nsr.begin(), nsr.end());
         
-        
         vector<int> width;
-        for(int i=0; i<nsl.size(); i++){
-            width.push_back((nsr[i]-nsl[i]-1)*heights[i]);
+        
+        for(int i=0; i<heights.size(); i++) {
+            width.push_back((nsr[i]-nsl[i]-1) * heights[i]);
         }
+        
         return *max_element(width.begin(), width.end());
         
     }
